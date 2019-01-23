72bb7978f5de6fca0b3e714fd60eacff2f042cfe
siddhant <ios.siddhant@gmail.com>
2019-01-23 12:57:31 +0530

    Initial Commit

---
+++ Sid'sToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sid'sToDoList
+//
+//  Created by Siddhant Mahapatra on 1/23/19.
+//  Copyright © 2019 Siddhant Mahapatra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Sid'sToDoListTests/Sid_sToDoListTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sid_sToDoListTests.swift
+//  Sid'sToDoListTests
+//
+//  Created by Siddhant Mahapatra on 1/23/19.
+//  Copyright © 2019 Siddhant Mahapatra. All rights reserved.
+//
+
+import XCTest
+@testable import Sid_sToDoList
+
+class Sid_sToDoListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
