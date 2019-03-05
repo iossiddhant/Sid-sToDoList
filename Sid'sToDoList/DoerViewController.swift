20eb51b1f19465a8b258b45e8a18e9ec281545f6
siddhant <ios.siddhant@gmail.com>
2019-03-05 17:37:27 +0530

    commit message

--- Sid'sToDoList/DoerViewController.swift
+++ Sid'sToDoList/DoerViewController.swift
@@ -7,47 +7,93 @@
 //
 
 import UIKit
+import CoreData
 
 class DoerViewController: UITableViewController {
 
-    var arrDoer = ["murli reading", "prepare food", "finish swift 4.2", "finish kotlin"]
+    @IBOutlet weak var searchBar: UISearchBar!
+    var arrDoer =  [DoerItem]()
+    var filteredArr = [DoerItem]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
    
+   // let filePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Doer.plist")
     // Here all my initialization code will be kept.
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-  
+        
+        self.fetchItems()
+        searchBar.delegate = self
+    }
+    
+    func deleteItem(at index: Int)  {
+        context.delete(filteredArr[index])
+        filteredArr.remove(at: index)
+    }
+    
+    func fetchItems()  {
+        var request:NSFetchRequest<DoerItem> = DoerItem.fetchRequest()
+        do {
+      arrDoer =   try context.fetch(request)
+    filteredArr = arrDoer
+            
+        }catch{
+            print("Error while Fetching..")
+            return
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    
+    func saveItems(title:String, isDone:Bool)  {
+        
+        var item = DoerItem(context: self.context)
+        item.title = title
+        item.done = isDone
+       self.saveContext()
+      
+
+    }
+    func saveContext()  {
+        do {
+            try self.context.save()
+        }catch
+        {
+            print("Error in Savin Data")
+        }
+         self.fetchItems()
     }
     //MARK: Here goes the tableview delegates which comes packed with uitableviewcontroller
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DoerCell", for: indexPath)
-        cell.textLabel?.text = arrDoer[indexPath.row]
+        cell.textLabel?.text = filteredArr[indexPath.row].title
+        cell.accessoryType = filteredArr[indexPath.row].done ? .checkmark : .none
         return cell
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrDoer.count
+        return filteredArr.count
     }
     
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
-       {
-        tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else
-       {
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+       // arrDoer[indexPath.row].done = !arrDoer[indexPath.row].done
+    self.deleteItem(at: indexPath.row)
+        tableView.deselectRow(at: indexPath, animated: true)
+        self.saveContext()
+        
+       
     }
 
     @IBAction func addItem(_ sender: UIBarButtonItem) {
         
        var textField = UITextField()
         var alert = UIAlertController(title: "Doer", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add Doer", style: .default) { (action) in
+        //  var path  = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+           // print(path[0])
             print("Succeed.")
-            self.arrDoer.append(textField.text!)
-            self.tableView.reloadData()
+          
+          self.saveItems(title: textField.text!, isDone: false)
         }
         alert.addAction(action)
         alert.addTextField { (alerttextField) in
@@ -63,3 +109,30 @@ class DoerViewController: UITableViewController {
     
 }
 
+
+extension DoerViewController: UISearchBarDelegate
+{
+   
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.count == 0
+        {
+            filteredArr = arrDoer
+            DispatchQueue.main.async {
+            searchBar.resignFirstResponder()
+            }
+           
+        }else
+        {
+            filteredArr.removeAll(keepingCapacity: false)
+            filteredArr = arrDoer.filter{ ($0.title?.localizedCaseInsensitiveContains(searchText))!}
+        }
+        self.tableView.reloadData()
+            
+            //arrDoer.filter { $0.title?.localizedCaseInsensitiveContains(searchBar.text)  }
+    }
+    
+}
